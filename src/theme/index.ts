d3ab519b257439a0712f9e543f18a4700b870177
Rashmiya <rashmiyarafeek1200@gmail.com>
2026-05-14 17:29:24 +0200

    chore(design-system): add theme & input barrel export for clean single line imports

---
+++ src/theme/index.ts
@@ -0,0 +1,5 @@
+// barrel file for theme-related exports
+export { colors } from "./colors";
+export { typography } from "./typography";
+export { spacing } from "./spacing";
+export { radius } from "./radius";
